6476cc6aa8a0f79635ca76e6b26b1f8a9c1a62d5
Eno1bas <E.Ibas@protonmail.com>
2025-05-16 16:56:27 +0100

    Migration of get.driver to HomePage

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -5,11 +5,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class HomePage {
+    public boolean goToAboutUsPage(WebDriver driver) {
+        driver.findElement(By.xpath("//*[@id='headerPanel']/ul[1]/li[2]/a")).click();
+        String titleText = driver.findElement(By.className("title")).getText();
+        return titleText.equals("ParaSoft Demo Website");
+    }
 
-
-    public void goToRegistrationPage(WebDriver driver) {
-
+    public RegistrationPage goToRegistrationPage(WebDriver driver) {
        WebElement element = driver.findElement(By.xpath("//*[@id='loginPanel']/p[2]/a"));
        element.click();
+       return new RegistrationPage();
+    }
+
+    public HomePage loadHomePage(WebDriver driver){
+        driver.get("https://parabank.parasoft.com/parabank/index.htm");
+        return new HomePage();
     }
 }
--- src/test/java/UI/Login.java
+++ src/test/java/UI/Login.java
@@ -9,12 +9,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -30,40 +27,24 @@ class Login {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         StringBuffer a = new StringBuffer();
-
     }
 
-
     @Test
-    void aboutUs() throws InterruptedException {
-        driver.get("https://parabank.parasoft.com/parabank/index.htm");
-        driver.findElement(By.xpath("//*[@id=\"headerPanel\"]/ul[1]/li[2]/a")).click();
-        assertTrue(driver.findElement(By.className("title")).getText().equals("ParaSoft Demo Website"));
+    void aboutUs() {
+        HomePage homePage = new HomePage().loadHomePage(driver);
+        boolean isOnAboutUsPage = homePage.goToAboutUsPage(driver);
+        assertTrue(isOnAboutUsPage);
         driver.quit();
     }
 
 
     @Test
-    void logIn() throws InterruptedException {
-        driver.get("https://parabank.parasoft.com/parabank/index.htm");
-        WebElement userNameField = driver.findElement(By.name("username"));
-        userNameField.sendKeys("EN0");
-        WebElement passwordField = driver.findElement(By.name("password"));
-        passwordField.sendKeys("QWERTY");
-        driver.findElement(By.xpath("//input[@value='Log In']")).click();
-        assertTrue(driver.findElement(By.className("smallText")).getText().contains("Eno Ibas"));
-    }
-
-    @Test
-    void registration() {
-        driver.get("https://parabank.parasoft.com/parabank/index.htm");
+    void registrationTest() {
+        HomePage homePage = new HomePage();
+        RegistrationPage regPage = homePage.loadHomePage(driver).goToRegistrationPage(driver);
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
-        HomePage homePage = new HomePage();
-        homePage.goToRegistrationPage(driver);
-
-        RegistrationPage regPage = new RegistrationPage();
         regPage.fillFields(driver, wait);
         regPage.submitRegistrationForm(driver, wait);
 
