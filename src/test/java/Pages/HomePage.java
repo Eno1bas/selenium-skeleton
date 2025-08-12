e758ab8942033f9b4bc09d61c6f2161ac3d12424
Eno1bas <E.Ibas@protonmail.com>
2025-08-12 16:22:09 +0100

    Corrected links and Shortened Tests

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -6,25 +6,34 @@ import org.openqa.selenium.WebElement;
 
 public class HomePage {
 
-    public RegistrationPage goToRegistrationPage(WebDriver driver) {
-       WebElement element = driver.findElement(By.xpath("//*[@id='loginPanel']/p[2]/a"));
-       element.click();
-       return new RegistrationPage();
-    }
-
-    public HomePage loadHomePage(WebDriver driver){
-        driver.get("https://parabank.parasoft.com/parabank/index.htm");
+    public HomePage loadHomePage(WebDriver driver) {
+        driver.get("https://para.testar.org/parabank/index.htm");
         return new HomePage();
     }
 
-    public AdminPage takeMeToAdminPage(WebDriver driver){
-        driver.findElement(By.xpath("/html/body/div[1]/div[2]/ul[1]/li[6]/a")).click();
+    public RegistrationPage goToRegistrationPage(WebDriver driver) {
+        WebElement element = driver.findElement(By.xpath("//*[@id='loginPanel']/p[2]/a"));
+        element.click();
+        return new RegistrationPage();
+    }
+
+    public AdminPage goToAdminPage(WebDriver driver) {
+        driver.findElement(By.linkText("Admin Page")).click();
         return new AdminPage();
     }
 
+    public ServicesPage goToServicePage(WebDriver driver) {
+        driver.findElement(By.linkText("Services")).click();
+        return new ServicesPage();
+    }
 
-    public ServicesPage takeMeToServicePage(WebDriver driver){
-        driver.findElement(By.xpath("/html/body/div[1]/div[2]/ul[1]/li[3]/a")).click();
-        return null;
+    public LocationsPage goToLocationPage(WebDriver driver) {
+        driver.findElement(By.linkText("Locations")).click();
+        return new LocationsPage();
+    }
+
+    public ProductsPage goToProductPage(WebDriver driver) {
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/ul[1]/li[4]/a")).click();
+        return new ProductsPage();
     }
 }
--- src/test/java/UI/LoginTest.java
+++ src/test/java/UI/LoginTest.java
@@ -1,6 +1,6 @@
 package UI;
 
-import Pages.RegistrationPage;
+import Pages.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -11,29 +11,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import Pages.HomePage;
-import Pages.*;
 
 @Nested
 class Login {
     WebDriver driver;
+    WebDriverWait wait;
 
     @BeforeEach
     void setupAll() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        StringBuffer a = new StringBuffer();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
-
 
     @Test
     void registrationTest() {
-        HomePage homePage = new HomePage();
-        RegistrationPage regPage = homePage.loadHomePage(driver).goToRegistrationPage(driver);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        RegistrationPage regPage = new HomePage().loadHomePage(driver).goToRegistrationPage(driver);
         regPage.fillFields(driver, wait);
         regPage.submitRegistrationForm(driver, wait);
         assertTrue(driver.findElement(By.tagName("h1")).getText().contains("Welcome Eno"));
@@ -41,20 +35,26 @@ class Login {
 
     @Test
     void servicePageTest() {
-        HomePage homePage = new HomePage();
-        ServicesPage servPage = homePage.loadHomePage(driver).takeMeToServicePage(driver);
-        driver.get("https://parabank.parasoft.com/parabank/index.htm");
-        servPage.goToServicePage(driver);
-        assertTrue( driver.findElement(By.className("porttypename")).getText().contains("Bookstore"));
+        new HomePage().loadHomePage(driver).goToServicePage(driver);
+        assertTrue(driver.findElement(By.className("heading")).getText().contains("Bookstore"));
     }
 
     @Test
-    void adminPageTest(){
-        HomePage homePage = new HomePage();
-        AdminPage abPage = homePage.loadHomePage(driver).takeMeToAdminPage(driver);
-        driver.get("https://parabank.parasoft.com/parabank/index.htm");
-       abPage.goToAdminPage(driver);
-        assertTrue(driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/table/tbody/tr/td[1]/h3")).getText().contains("Database"));
+    void adminPageTest() {
+        new HomePage().loadHomePage(driver).goToAdminPage(driver);
+        assertTrue(driver.findElement(By.className("Title")).getText().contains("Administration"));
+    }
+
+    @Test
+    void locationPageTest() {
+        new HomePage().loadHomePage(driver).goToLocationPage(driver);
+        assertTrue(driver.findElement(By.className("wysiwyg-content")).getText().contains("Safety & Security-Critical Testing"));
+    }
+
+    @Test
+    void productsPage() {
+        new HomePage().loadHomePage(driver).goToProductPage(driver);
+        assertTrue(driver.findElement(By.className("cky-preference-title")).getText().contains("Customise Consent Preferences"));
     }
 }
 
